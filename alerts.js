a2f3b4d3d64f75d9293f55c27f43ecc91277aac5
User8395 <therealuser8395@proton.me>
2024-12-03 18:29:36 -0500

    Add alerts

--- alerts.js
+++ alerts.js
@@ -1,3 +1,17 @@
-const linealerts = []
+const linealerts = [
+    {
+        "lines": ["IT2"],
+        "title": "No service to station",
+        "status": "Planned",
+        "description": "Starting December 7, there will be no service to London Central due to improper slopes at the north end.<br>Free shuttle buses from Swamp Village will be provided."
+    }
+]
 
-const stationalerts = []
+const stationalerts = [
+    {
+        "station": "London Central",
+        "title": "No service to station",
+        "status": "Planned",
+        "description": "Starting December 7, there will be no service to London Central due to improper slopes at the north end.<br>Free shuttle buses from Swamp Village will be provided."
+    }
+]
