33bc55f58084bcdfea2f383382378504aba2e2f7
TheReal User8395 <therealuser8395@proton.me>
2024-12-03 15:12:03 -0500

    update

--- lines.js
+++ lines.js
@@ -47,36 +47,3 @@ const lines = [
         ]
     }
 ]
-
-const stations = [
-    {
-        "name": "New Karachi",
-        "platforms": "6",
-        "lines": ["IT1", "IT2"]
-    },
-    {
-        "name": "London Central",
-        "platforms": "4?",
-        "lines": ["IT1", "IT2"]
-    },
-    {
-        "name": "Hammerbrook South",
-        "platforms": "2",
-        "lines": ["IT2"]
-    },
-    {
-        "name": "Hammerbrook North",
-        "platforms": "2",
-        "lines": ["IT2"]
-    },
-    {
-        "name": "Swamp Village",
-        "platforms": "4",
-        "lines": ["IT2"]
-    },
-    {
-        "name": "Pinewood City",
-        "platforms": "3",
-        "lines": ["IT1"]
-    }
-]
---
+++ stations.js
@@ -0,0 +1,32 @@
+var stations = [
+    {
+        "name": "New Karachi",
+        "platforms": "2",
+        "lines": ["IT1", "IT2"]
+    },
+    {
+        "name": "London Central",
+        "platforms": "4?",
+        "lines": ["IT1", "IT2"]
+    },
+    {
+        "name": "Hammerbrook South",
+        "platforms": "2",
+        "lines": ["IT2"]
+    },
+    {
+        "name": "Hammerbrook North",
+        "platforms": "2",
+        "lines": ["IT2"]
+    },
+    {
+        "name": "Swamp Village",
+        "platforms": "4",
+        "lines": ["IT2"]
+    },
+    {
+        "name": "Pinewood City",
+        "platforms": "3",
+        "lines": ["IT1"]
+    }
+]
