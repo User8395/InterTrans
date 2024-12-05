99395d3b506fb1c7317200cbe87488076ed27ace
User8395 <therealuser8395@proton.me>
2024-12-05 16:13:20 -0500

    alerts

--- alerts.js
+++ alerts.js
@@ -1,17 +1,17 @@
 const linealerts = [
-    {
-        "lines": ["IT2"],
-        "title": "No service to station",
-        "status": "Planned",
-        "description": "Starting December 7, there will be no service to London Central due to improper slopes at the north end.<br>Free shuttle buses from Swamp Village will be provided."
-    }
+    // {
+    //     "lines": ["IT2"],
+    //     "title": "No service to station",
+    //     "status": "Planned",
+    //     "description": "Starting December 7, there will be no service to London Central due to improper slopes at the north end.<br>Free shuttle buses from Swamp Village will be provided."
+    // }
 ]
 
 const stationalerts = [
-    {
-        "station": "London Central",
-        "title": "No service to station",
-        "status": "Planned",
-        "description": "Starting December 7, there will be no service to London Central due to improper slopes at the north end.<br>Free shuttle buses from Swamp Village will be provided."
-    }
+    // {
+    //     "station": "London Central",
+    //     "title": "No service to station",
+    //     "status": "Planned",
+    //     "description": "Starting December 7, there will be no service to London Central due to improper slopes at the north end.<br>Free shuttle buses from Swamp Village will be provided."
+    // }
 ]
