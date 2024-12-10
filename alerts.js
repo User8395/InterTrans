74d094d7d9ee1cbfd879c5087aa14a217319d273
User8395 <therealuser8395@proton.me>
2024-12-10 15:59:32 -0500

    fix bugs and add alerts

--- alerts.js
+++ alerts.js
@@ -1,13 +1,13 @@
 const linealerts = [
     {
         "lines": ["IT1"],
-        "title": "Delays: Few trains available",
+        "title": "Delays - Few trains available",
         "status": "Active",
         "description": "IT1 trains heading both directions will be delayed due to few trains in service."
     },
     {
         "lines": ["IT2"],
-        "title": "Delays: No service ",
+        "title": "Delays - No service in direction",
         "status": "Active",
         "description": "IT2 trains will only be heading on Direction 1 due to the few trains in service."
     }
