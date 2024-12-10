789cd4d9db7722aecb490fbb6f9efe1a771c759d
User8395 <therealuser8395@proton.me>
2024-12-10 15:42:22 -0500

    add alerts

--- alerts.js
+++ alerts.js
@@ -1,10 +1,16 @@
 const linealerts = [
-    // {
-    //     "lines": ["IT2"],
-    //     "title": "No service to station",
-    //     "status": "Planned",
-    //     "description": "Starting December 7, there will be no service to London Central due to improper slopes at the north end.<br>Free shuttle buses from Swamp Village will be provided."
-    // }
+    {
+        "lines": ["IT1"],
+        "title": "Delays: Few trains available",
+        "status": "Active",
+        "description": "IT1 trains heading both directions will be delayed due to few trains in service."
+    },
+    {
+        "lines": ["IT2"],
+        "title": "Delays: No service ",
+        "status": "Active",
+        "description": "IT2 trains will only be heading on Direction 1 due to the few trains in service."
+    }
 ]
 
 const stationalerts = [
